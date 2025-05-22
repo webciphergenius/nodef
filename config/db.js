21c2bdc42c53608d9c618ba75cd9837c6a8edac2
Devender Kumar <lucdev93@gmail.com>
2025-05-22 22:35:31 +0530

    checking db issue

--- config/db.js
+++ config/db.js
@@ -1,6 +1,11 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
-
+console.log("DB ENV:", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
